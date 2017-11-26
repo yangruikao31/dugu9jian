9bd235ae44db65688589895910d7cd90dc9be322
yangruikao33 <yangruikao32@126.com>
2017-11-26 21:04:35 +0800

    dugu10jian

--- src/com/atguigu/git/Dugu9jian.java
+++ src/com/atguigu/git/Dugu9jian.java
@@ -2,6 +2,10 @@ package com.atguigu.git;
 
 public class Dugu9jian {
 	public static void main(String[] args) {
-		System.out.println("Hello dugu9jian");
+		System.out.println("Hello dugu10jian");
+		System.out.println("Hello dugu10jian");
+		System.out.println("Hello dugu10jian");
+		System.out.println("Hello dugu10jian");
+		
 	}
 }
